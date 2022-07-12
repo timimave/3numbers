a4c47401fe44a625c9efe7a435c1277dc29f408d
timimave <73471414+timimave@users.noreply.github.com>
2022-07-12 23:05:16 +0300

    master

---
+++ StringCalculator.java
@@ -0,0 +1,88 @@
+package MyGit;
+
+import java.util.Scanner;
+
+public class StringCalculator {
+    static Scanner scanner = new Scanner(System.in);
+    static String operator;
+
+
+    public static void main(String[] args) {
+        startOperation();
+    }
+
+    public static void startOperation() {
+        String operationResult = getOperation();
+        if (operationResult.length() > 40) {
+            String result = operationResult.substring(40);
+            result = operationResult.replaceFirst(result, "") + "...";
+            System.out.println(result);
+        } else {
+            System.out.println(operationResult);
+        }
+    }
+
+    public static String getOperation() {
+        String str = scanner.nextLine();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.contains("+")) operator = "+";
+            if (str.contains("-")) operator = "-";
+            if (str.contains("*")) operator = "*";
+            if (str.contains("/")) operator = "/";
+
+        }
+        String[] splitter = str.split("[+\\-*/]");
+
+        String a = splitter[0].replaceAll(" ", "");
+        String b = splitter[1].replaceAll(" ", "");
+        return calculate(a, operator, b);
+
+    }
+
+    public static String calculate(String a, String operator, String b) {
+      try {
+          return switch (operator) {
+              case "+" -> a.concat(b);
+              case "-" -> a.replaceFirst(b, "");
+              case "*" -> a.repeat(Integer.parseInt(b));
+              case "/" -> a.substring(0, a.length() / Integer.parseInt(b));
+              default -> null;
+          };
+      } catch (Exception exception) {
+          System.out.println("Неверное действие: " + exception);
+      }
+      return null;
+    }
+}
+
+
+/*Пример работы программы:
+Input:
+"100" + "500"
+
+Output:
+"100500"
+
+Input:
+"Hi World!" - "World!"
+
+Output:
+"Hi "
+
+Input:
+"Bye-bye!" - "World!"
+
+Output:
+"Bye-bye!"
+
+Input:
+"Java" * 5
+
+Output:
+"JavaJavaJavaJavaJava"
+
+Input:
+"Example!!!" / 3
+
+Output:
+"Exa"*/
